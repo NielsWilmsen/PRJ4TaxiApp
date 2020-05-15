4c899c8be123b754b454e1ea239301387a7a9859
Radu-Mihail Rusu <45173646+radu3154@users.noreply.github.com>
2020-05-15 15:34:36 +0300

    added the open camera button
    For now, just takes the photo and inserts it on the page

--- TaxiApp/TaxiApp/ViewControllers/CustomerRegisterViewController.swift
+++ TaxiApp/TaxiApp/ViewControllers/CustomerRegisterViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-class CustomerRegisterViewController: UIViewController, ResponseHandler {
+class CustomerRegisterViewController: UIViewController, ResponseHandler,UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
     
     // Outlet declaration
     @IBOutlet weak var nameText: UITextField!
@@ -8,6 +9,8 @@ class CustomerRegisterViewController: UIViewController, ResponseHandler {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     
+    //Test purpose
+    @IBOutlet var profilePicture: [UIImageView]!
     let restAPI = RestAPI()
     
     override func viewDidLoad() {
@@ -15,6 +18,34 @@ class CustomerRegisterViewController: UIViewController, ResponseHandler {
         
         // Dismiss the keyboard when a user taps anywhere
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard)))
+    }
+    
+    //This is the function to open the camera
+    @IBAction func takePicture(_ sender: AnyObject) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    
+    //This is the function that takes the image and processes it
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+
+        //Just to test, I added an image view to show the image
+        let myImageView:UIImageView = UIImageView()
+        myImageView.contentMode = UIView.ContentMode.scaleAspectFit
+        myImageView.frame.size.width = 200
+        myImageView.frame.size.height = 200
+        myImageView.center = self.view.center
+        myImageView.image = image
+        view.addSubview(myImageView)
     }
     
     @IBAction func register(_ sender: UIButton) {
@@ -41,6 +72,8 @@ class CustomerRegisterViewController: UIViewController, ResponseHandler {
         let parameters = ["first_name": name, "last_name": lastName, "email": email, "password": password] as [String : String]
         
         restAPI.post(parameters, "/customers")
+        
+        
     }
     
     @objc func dismissKeyboard(){
