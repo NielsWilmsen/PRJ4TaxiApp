8e946518c51da930243a167c839a7d30ded4222e
Niels Wilmsen <nielswilmsen@Nielss-MacBook-Pro.local>
2020-05-28 14:39:51 +0200

    Can now post with any type

--- TaxiApp/TaxiApp/Classes/Order.swift
+++ TaxiApp/TaxiApp/Classes/Order.swift
@@ -9,14 +9,16 @@ class Order {
     var customerEmail: String!
     var driverEmail: String!
     var fare: Int!
+    var status: Int!
     
-    init(_ pickup: String, _ destination: String, _ customer: String, _ driver: String, _ fare: Int, _ id: Int){
+    init(_ pickup: String, _ destination: String, _ customer: String, _ driver: String, _ fare: Int, _ id: Int, _ status: Int){
         self.pickup = pickup
         self.destination = destination
         self.customerEmail = customer
         self.driverEmail = driver
         self.id = id
         self.fare = fare
+        self.status = status
     }
     
 }
